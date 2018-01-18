9fcd33b0b8271f0681ad7ea88073108513bb3cbb
theBEERd <drumbum4life@gmail.com>
2018-01-17 23:14:41 -0800

    Assignments 3, Gaddis ch4, Proj 13

--- Hmwk/Assignments_3/Savitch_10_ch3_13_holy-numbers-batman!/main.cpp
+++ Hmwk/Assignments_3/Savitch_10_ch3_13_holy-numbers-batman!/main.cpp
@@ -22,37 +22,39 @@ using namespace std;
  */
 int main(int argc, char** argv) {
     int ones, tens, hundreds, thousands, address;
+    bool found = false;
     
-    ones = tens = hundreds = thousands = address = 0;
-    bool run = true;
-    
-    // Loop ones 0-9
-    for (ones; ones <= 9; ones++) {
-        tens = tens == 10 ? 0 : ones+1;
-        // Loop tens 0-9
-        for (tens; tens <= 9; tens++) {
-            thousands = thousands == 0 ? tens+3 : tens*3;
+    // Loop ones 1-9
+    for (ones = 1; ones < 10; ones++) {
+        // Loop tens 2-9
+        for (tens = 2; tens <= 9; tens++) {
+            thousands = tens*3;
         
             if (thousands > 9) {
                 cout << "Address not found... thousands\n";
+                break;
             }
 
-            hundreds = 27-thousands-tens-ones;
+            hundreds = 27-(thousands+tens+ones);
             if (hundreds <= 9) {
                 if (hundreds != ones && hundreds != tens && hundreds != thousands) {
                     cout << "Address found!!\n";
                     cout << "The address is: " << thousands << hundreds << tens << ones << endl;
+                    found = true;
                     break;
                 }
                 else {
                     cout << "Address not found... hundreds similar\n";
+                    
                 }
             }
             else {
                 cout << "Address not found... hundreds greater than 9\n";
             }
         }
-        
+        if (found) {
+            break;
+        }
     }
 
     return 0;
